5adcf3dc141fe0f0560f49e3300e7a102af589bc
Benjamin Anderson <benjaminghray@gmail.com>
2021-01-12 16:58:05 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+    'My name is Ben'
+
+    end
+    get '/hometown' do
+        'My hometown is Longview TX'
+    end
+    get '/favorite-song' do
+    'My favorite song is Time - ZRo'
+    end
 end
